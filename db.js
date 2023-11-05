57076b8387264c48f473b7bf2c3f15c1aaf4c0d5
Prajwal <prajwalkulkarni766@gmail.com>
2023-11-05 13:29:28 +0530

    used dotenv

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
-// database uri
-const mongoURI = 'mongodb://0.0.0.0:27017/example';
+const dotenv = require("dotenv");
+dotenv.config();
+
+// mongodb uri
+const mongoURI = process.env.MONGO_URI;
 
 const ConnectToMongoose = async () => {
     try {
@@ -8,7 +11,7 @@ const ConnectToMongoose = async () => {
         console.log('connected');
     }
     catch (err) {
-        console.log('failed',err);
+        console.log('failed to connect with database',err);
     }
 }
 
